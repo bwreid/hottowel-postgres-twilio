278daa2d8a601bd5b0e3c24825ab6e702afca9e2
Wes Reid <bwreid@gmail.com>
2015-11-07 12:21:37 -0700

    mock out matching
    * added matching logic to dashboard controller
    * made changes to dashboard.html to show and remove matches

--- src/client/app/states/dashboard/dashboard.controller.js
+++ src/client/app/states/dashboard/dashboard.controller.js
@@ -1,41 +1,90 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app.dashboard')
-        .controller('DashboardController', DashboardController);
+  angular
+  .module('app.dashboard')
+  .controller('DashboardController', DashboardController);
 
-    DashboardController.$inject = ['$q', 'dataservice', 'logger'];
-    /* @ngInject */
-    function DashboardController($q, dataservice, logger) {
-        var vm = this;
-        vm.news = {
-            title: 'hottowelPostgresTwilio',
-            description: 'Hot Towel Angular is a SPA template for Angular developers.'
-        };
-        vm.messageCount = 0;
-        vm.people = [];
-        vm.title = 'Dashboard';
+  DashboardController.$inject = ['$q', 'dataservice', 'logger'];
+  /* @ngInject */
+  function DashboardController($q, dataservice, logger) {
+    var vm = this;
+    vm.news = {
+      title: 'hottowelPostgresTwilio',
+      description: 'Hot Towel Angular is a SPA template for Angular developers.'
+    };
+    vm.people = [];
+    vm.title = 'Dashboard';
 
-        activate();
+    activate();
 
-        function activate() {
-            var promises = [getMessageCount(), getPeople()];
-            return $q.all(promises);
-        }
-
-        function getMessageCount() {
-            return dataservice.getMessageCount().then(function (data) {
-                vm.messageCount = data;
-                return vm.messageCount;
-            });
-        }
-
-        function getPeople() {
-            return dataservice.getPeople().then(function (data) {
-                vm.people = data;
-                return vm.people;
-            });
-        }
+    function activate () {
+      var promises = [getPeople()];
+      return $q.all(promises);
     }
+
+    function getPeople () {
+      return dataservice.getPeople().then(function (data) {
+        vm.people = data;
+        return vm.people;
+      });
+    }
+
+    vm.selectPerson = selectPerson;
+
+    function selectPerson (username, selection) {
+      var selected = vm.people.filter(function (person) {
+        return person.username == username;
+      })[0];
+
+      selected.status = selection;
+
+      if ( selected.status == selected.likesYou ) {
+        vm.matchedPerson = selected;
+        $('#myModal').modal('show');
+      }
+    }
+
+    vm.connectUsers = connectUsers;
+    
+    function connectUsers (user) {
+      var connectedUserEmail = $('#matchEmail').val();
+      var connectedUserPhone = $('#matchPhone').val();
+
+      vm.warnings = {};
+      if ( !(connectedUserEmail || connectedUserPhone) ) {
+        vm.warnings.required = 'Email or phone is required to send a message.'
+      }
+
+      if ( connectedUserEmail && !validator.isEmail(connectedUserEmail) ) {
+        vm.warnings.email = 'Your email is in an incorrect format.';
+      }
+
+      if ( connectedUserPhone && !validator.isEmail(connectedUserPhone) ) {
+        vm.warnings.phone = 'Your phone is in an incorrect format. (Only numbers)';
+      }
+
+      if ( !vm.warnings.length && ( connectedUserEmail || connectedUserPhone ) ) {
+        if ( connectedUserEmail ) {
+          console.log('Sending email to', connectedUserEmail);
+        }
+
+        if ( connectedUserPhone ) {
+          console.log('Sending text message to', connectedUserPhone);
+        }
+
+        if ( user ) {
+          if ( user.email ) {
+            console.log('Copying on the email', user.email);
+          }
+
+          if ( connectedUserPhone ) {
+            console.log('Copying on the email', connectedUserPhone);
+          }
+          
+        }
+        $('#myModal').modal('hide');
+      }
+    }
+  }
 })();
